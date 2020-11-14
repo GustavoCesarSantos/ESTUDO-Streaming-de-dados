29a7ca7226a352042e73ecbb7dfcbc021a8214df
GustavoCesarSantos <gustavocs789@gmail.com>
2020-11-13 22:53:16 -0300

    Criado escrita do arquivo utilizando stream

--- arquivos/uploadDeArquivos.js
+++ arquivos/uploadDeArquivos.js
@@ -4,6 +4,10 @@ fs.readFile('./assets/download.jpg', (err, buffer) => {
   console.log(buffer);
 
   fs.writeFile('./assets/teste.jpg', buffer, (err) => {
-    console.log('Imagem escrita com sucesso.');
+    console.log('Imagem escrita com sucesso. - Buffer');
   })
 })
+
+fs.createReadStream('./assets/download.jpg')
+  .pipe(fs.createWriteStream('./assets/testeStream.jpg'))
+  .on('finish', () => console.log('Imagem escrita com sucesso. - Stream'))
